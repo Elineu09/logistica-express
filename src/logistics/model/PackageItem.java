6fb916d6f6aace4d34860482076104619fea7a16
Luís Dalton Velinho de Almeida <20230615@isptec.co.ao>
2025-12-16 23:36:43 +0100

    Update PackageItem.java

--- src/logistics/model/PackageItem.java
+++ src/logistics/model/PackageItem.java
@@ -1,7 +1,19 @@
 package logistics.model;
 
 public class PackageItem {
-        public static void test(){
-        System.out.println("Package Item!");
+        private int weight;
+        private int value;
+
+    public PackageItem(int weigth, int value) {
+        this.weigth = weigth;
+        this.value = value;
+    }
+
+    public int getWeigth() {
+        return weigth;
+    }
+
+    public int getValue() {
+        return value;
     }
 }
