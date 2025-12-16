981fef7f37888816ffd091805b1a4b0ef2096d33
Elineu <elineutomas4@gmail.com>
2025-12-17 00:50:03 +0100

    Update knapsack and util

--- src/logistics/util/PerformanceMonitor.java
+++ src/logistics/util/PerformanceMonitor.java
@@ -1,7 +1,25 @@
 package logistics.util;
 
 public class PerformanceMonitor {
-        public static void test(){
-        System.out.println("Performance Monitor!");
+    private long startTime;
+    private long startMemory;
+
+    public void start() {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        startMemory = runtime.totalMemory() - runtime.freeMemory();
+        startTime = System.nanoTime();
+    }
+
+    public void stop(){
+        long endTime = System.nanoTime();
+        Runtime runtime = Runtime.getRuntime();
+        long endMemory = runtime.totalMemory() - runtime.freeMemory();
+
+        double timeMs = (endTime - startTime) / 1_000_000.0;
+        double memoryMB = (endMemory - startMemory) / (1024.0 * 1024.0);
+
+        System.out.println("Tempo de execução: " + timeMs + " ms");
+        System.out.println("Memória utilizada: " + memoryMB + " MB");
     }
 }
